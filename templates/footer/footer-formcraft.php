1890843401d9244c63e17f041e45c0acf794bdd9
Alex Ballera <alexballera@gmail.com>
2017-02-10 21:32:14 -0400

    Terminado footer

--- templates/footer/footer-formcraft.php
+++ templates/footer/footer-formcraft.php
@@ -1,10 +1,10 @@
 <?php 
 
 if (function_exists("add_formcraft_form")) {
-	if  (get_bloginfo( 'url' ) === "http://localhost/frio") {
+	if  (get_bloginfo( 'url' ) === "http://localhost/colorglob") {
 		add_formcraft_form("[fc id='1'][/fc]");
 	} else { 
-		add_formcraft_form("[fc id='1'][/fc]");
+		add_formcraft_form("[fc id='3'][/fc]");
 	}
 } else {
 	echo "<p style='background:red;color:yellow;text-align:center'>AGREGA FORMCRAFT</p>";
